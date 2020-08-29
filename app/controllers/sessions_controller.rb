fe00b1607a5e9fa3e9cbf9711638d6613bceb7bb
Chance Smith <chancesmith97@hotmail.com>
2020-08-29 01:44:51 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,25 @@
 class SessionsController < ApplicationController
   before_action :require_login, only: [:show]
 
+  def index
+    if current_user
+      redirect_to '/welcome'
+    else
+      redirect_to '/login'
+    end
+  end
+
 
   def new
   end
 
   def create
+    if params[:name] == "" || params[:name] == nil
+      redirect_to '/login'
+    else
+      session[:name] = params[:name]
+      redirect_to '/welcome'
+    end
   end
 
 
