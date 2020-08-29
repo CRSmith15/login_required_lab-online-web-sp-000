036f92c06b72b125c509cba8d326dac6b3550168
Chance Smith <chancesmith97@hotmail.com>
2020-08-29 01:50:57 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
-  root 'application#welcome'
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
+  get '/welcome' => 'sessions#show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
