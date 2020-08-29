e389d40ec351c19294f318630461b308e2bfd0bf
Chance Smith <chancesmith97@hotmail.com>
2020-08-29 01:28:05 +0000

    Automatically backed up by Learn

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController 
+  before_action :require_login
+
+  def new 
+  end
+
+  def create 
+  end
+
+  private 
+
+  def require_login 
+    return head(:forbidden) unless session.include? :user_id 
+  end
+
+end
